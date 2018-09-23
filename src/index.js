94a6daeb7c6bc0cfd30397107b3bf8c2d5b4a3aa
Connie Leung <cleung334@gmail.com>
2018-09-23 22:09:26 +0800

    Add footer to index.html
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import nav from "./nav";
-import { top, bottom } from "./footer";
+import { footer } from "./footer";
 import makeButton from "./button";
 import { makeColorStyle } from "./button-styles";
 
 const button = makeButton("Yay! A first button");
-const button2 = makeButton("Yay! A second button");
+button.style = makeColorStyle("cyan");
 
 document.body.appendChild(button);
-document.body.appendChild(button2);
-
-console.log(nav(), top, bottom, makeColorStyle("cyan"));
+document.body.appendChild(footer);
