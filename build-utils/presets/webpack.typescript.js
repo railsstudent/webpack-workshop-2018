a270bd7c28d13af1a82b10f53881f4d2a58976fe
Connie Leung <connie.leung@nixplay.com>
2018-09-24 13:16:00 +0800

    WIP: add typescript presets
    Signed-off-by: Connie Leung <connie.leung@nixplay.com>

---
+++ build-utils/presets/webpack.typescript.js
@@ -0,0 +1,9 @@
+module.exports = () => ({
+    module: {
+        rules: [
+            {   test: /\.ts$/, 
+                use: ["ts-loader"] 
+            }
+        ]
+    }  
+});
---
+++ src/foo.ts
@@ -0,0 +1,7 @@
+export default class Foo {
+    public bar;
+    // constructor() {
+    //     console.log("testing");
+    //     this.bar = "hi";
+    // }
+}
