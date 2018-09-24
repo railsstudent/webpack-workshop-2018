5de95001cb6c3b0cde1a493e13b371913cc49b3b
Connie Leung <cleung334@gmail.com>
2018-09-24 20:51:51 +0800

    Support presets to load at different environemnt mode
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpackMerge = require("webpack-merge");
 const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
+const presetConfig = require("./build-utils/loadPresets");
 
 module.exports = ({ mode, presets } = { mode: "production", presets: [] }) => {
   return webpackMerge(
@@ -23,6 +24,7 @@ module.exports = ({ mode, presets } = { mode: "production", presets: [] }) => {
       },
       plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()]
     },
-    modeConfig(mode)
+    modeConfig(mode),
+    presetConfig({mode, presets})
   );
 };
